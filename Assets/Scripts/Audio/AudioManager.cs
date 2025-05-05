eb7e3bf34df6aded33e199f135b48f3b8115c775
Linamcaro <lina.mcaro@gmail.com>
2025-05-05 15:02:47 -0500

    FMOD

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,54 @@
+
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [SerializeField] EventReference musicEventMenu; // Reference to the music event in FMOD
+    [SerializeField] EventReference musicEventGame; // Reference to the music event in FMOD
+    [SerializeField] float rate;
+    [SerializeField] GameObject GameObjectReference; // Pitch of the music
+                                                     // [SerializeField] EventReference sfxEvent; // Reference to the SFX event in FMOD  
+
+
+    private EventInstance musicInstance;
+
+    void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == LoadScenes.SceneName.MainMenu.ToString())
+        {
+            PlayMusic(musicEventMenu);
+        }
+        else if (SceneManager.GetActiveScene().name == LoadScenes.SceneName.NetworkedPrototype.ToString())
+        {
+            PlayMusic(musicEventGame);
+        }
+    }
+
+    public void PlayMusic(EventReference eventReference)
+    {
+        musicInstance = RuntimeManager.CreateInstance(eventReference);
+        musicInstance.start();
+
+
+    }
+
+    void OnDestroy()
+    {
+        musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        musicInstance.release();
+    }
+
+}
